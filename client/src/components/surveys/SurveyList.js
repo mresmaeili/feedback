595f11bbd19ef771bcd276fbe87fc11cac604f61
mrEsmaeili <lvlr.esmaeili@gmail.com>
2019-11-04 20:04:24 +0300

    working on dev fine

--- client/src/components/surveys/SurveyList.js
+++ client/src/components/surveys/SurveyList.js
@@ -9,17 +9,17 @@ class SurveyList extends Component {
   renderSurveys() {
     return this.props.surveys.reverse().map(survey => {
       return (
-        <div class='card blue-grey darken-1' key={survey._id}>
-          <div class='card-content white-text'>
-            <span class='card-title'>{survey.title}</span>
+        <div className='card blue-grey darken-1' key={survey._id}>
+          <div className='card-content white-text'>
+            <span className='card-title'>{survey.title}</span>
             <p>{survey.body}</p>
             <p className='right'>
               Sent On: {new Date(survey.dateSent).toLocaleDateString()}
             </p>
           </div>
           <div className='card-action'>
-            <a>YES: {survey.yes}</a>
-            <a>NO: {survey.no}</a>
+            <a href='#!'>YES: {survey.yes}</a>
+            <a href='#!'>NO: {survey.no}</a>
           </div>
         </div>
       );
--- index.js
+++ index.js
@@ -18,8 +18,7 @@ app.use(bodyParser.json());
 app.use(
   cookieSession({
     maxAge: 30 * 24 * 60 * 60 * 1000,
-    keys: [keys.cookieKey],
-    secure
+    keys: [keys.cookieKey]
   })
 );
 
