3f5905f8b0c3adbe8f9934564fc261a59968ff59
Mostafa <mostafa@larsa.org>
2022-08-26 19:28:22 +0430

    mini ups to hroku v 22

--- client/src/components/Dashboard.js
+++ client/src/components/Dashboard.js
@@ -1,14 +1,14 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
-import SurveyList from './surveys/SurveyList';
+import React from "react";
+import { Link } from "react-router-dom";
+import SurveyList from "./surveys/SurveyList";
 
 export default function Dashboard() {
   return (
     <div>
       <SurveyList />
-      <div className='fixed-action-btn'>
-        <Link to='surveys/new' className='btn-floating btn-large red'>
-          <i className='large material-icons'>add</i>
+      <div className="fixed-action-btn">
+        <Link to="surveys/new" className="btn-floating btn-large red">
+          <i className="large material-icons pulse">add</i>
         </Link>
       </div>
     </div>
--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -1,7 +1,7 @@
-import React, { Component } from 'react';
-import { Link } from 'react-router-dom';
-import { connect } from 'react-redux';
-import Payments from './Payments';
+import React, { Component } from "react";
+import { Link } from "react-router-dom";
+import { connect } from "react-redux";
+import Payments from "./Payments";
 
 class Header extends Component {
   renderLogin() {
@@ -11,39 +11,44 @@ class Header extends Component {
       case false:
         return (
           <li>
-            <a href='/auth/google'>Login with Google</a>
+            <a href="/auth/google">Login with Google</a>
           </li>
         );
       default:
         return [
-          <li key='1'>
+          <li key="1">
             <Payments />
           </li>,
-          <li key='3' style={{ margin: '0 10px' }}>
-            {' '}
+          <li key="3" style={{ margin: "0 10px" }}>
+            {" "}
             Credits: {this.props.auth.credits}
           </li>,
-          <li key='2'>
-            <a href='/api/logout'>Logout</a>
-          </li>
+          <li key="2">
+            <a href="/api/logout">Logout</a>
+          </li>,
         ];
     }
   }
   render() {
     return (
-      <nav>
-        <div className='nav-wrapper'>
-          <Link
-            to={this.props.auth ? '/surveys' : '/'}
-            className='left brand-logo'
-          >
-            Feedback Maker
-          </Link>
-          <ul id='nav-mobile' className='right'>
-            {this.renderLogin()}
-          </ul>
+      <div className="row">
+        <div className="col s12 m12">
+          <nav>
+            <div className="nav-wrapper">
+              <Link
+                to={this.props.auth ? "/surveys" : "/"}
+                className="left brand-logo"
+                style={{ paddingLeft: 12 }}
+              >
+                Feedback Maker
+              </Link>
+              <ul id="nav-mobile" className="right">
+                {this.renderLogin()}
+              </ul>
+            </div>
+          </nav>
         </div>
-      </nav>
+      </div>
     );
   }
 }
